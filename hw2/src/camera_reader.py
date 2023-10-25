4f05edcdd80ae0e28a1e4c9f14d1b188ee905654
txff99 <txfff99@gmail.com>
2023-10-25 09:12:07 +0000

    [hw2] add: image reader

---
+++ hw2/src/camera_reader.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+
+if __name__ == "__main__":
+    bridge = CvBridge()
+    rospy.init_node("image_reader")
+    raw_image_msg = rospy.wait_for_message("/camera_0", Image, timeout=1.0)
+    print(raw_image_msg.encoding)
+    cv_image = bridge.imgmsg_to_cv2(raw_image_msg, desired_encoding='passthrough')
+    (rows,cols,channels) = cv_image.shape
+    print("rows: {}, cols: {}, channels: {}".format(rows, cols, channels))
+    cv2.imshow("Image window", cv_image)
+    cv2.waitKey(0)
